92d3d164a92e5db66cf3252a7f7ba9d9a0b6a9ea
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-14 18:29:15 -0500

    Fix horizontal overflow on mobile

--- _app/immutable/chunks/Chz3_k1H.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1os4xi7?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1os4xi7?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/sNzIgJJX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1m3ykgl?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1m3ykgl?.assets??s??"";export{a,s as b};
