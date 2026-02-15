b713a7170106671f97e89c0c68e4bbe1c74957c6
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-15 12:34:08 -0500

    Add Spanish translations (exampleEs) to glossary examples

---
+++ _app/immutable/chunks/TABkglUg.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_vqmd3w?.base??"/grammar-cards-game",a=globalThis.__sveltekit_vqmd3w?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.Cwu48hyE.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/gf_upjOK.js";export{o as load_css,r as start};
