f96192803980186a6077ffded4c9d131162b0b9b
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-16 11:39:25 -0500

    Lower temperature to 0.5 to fix hint/sentence subject mismatch

--- _app/immutable/chunks/BPQHLof8.js
+++
@@ -1,1 +0,0 @@
-const a=globalThis.__sveltekit_1haa6xj?.base??"/grammar-cards-game",s=globalThis.__sveltekit_1haa6xj?.assets??a??"";export{s as a,a as b};
---
+++ _app/immutable/chunks/D5crDMnE.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_nbt6g?.base??"/grammar-cards-game",a=globalThis.__sveltekit_nbt6g?.assets??s??"";export{a,s as b};
