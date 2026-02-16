95e1f9f870b887c540cef7b32d39a08fe64367d5
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-16 11:32:35 -0500

    Add text-to-speech pronunciation for words and sentences

--- _app/immutable/chunks/9jTDW8Zd.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1e3e8fy?.base??"/grammar-cards-game",e=globalThis.__sveltekit_1e3e8fy?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/BPQHLof8.js
@@ -0,0 +1,1 @@
+const a=globalThis.__sveltekit_1haa6xj?.base??"/grammar-cards-game",s=globalThis.__sveltekit_1haa6xj?.assets??a??"";export{s as a,a as b};
