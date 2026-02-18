e2a49f6d9b9f99aaa00e7935f6d9966bb14236a6
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-18 18:09:43 -0500

    Add 5 new tenses + UI text fix

--- _app/immutable/chunks/CULcjerN.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1sc6wg?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1sc6wg?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/CWHwrSL5.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_j6oac2?.base??"/grammar-cards-game",a=globalThis.__sveltekit_j6oac2?.assets??s??"";export{a,s as b};
