8b0be1c641267f462685bed7d630ba093b030728
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-18 20:28:46 -0500

    Require natural realistic sentences in exercises

--- _app/immutable/chunks/CWHwrSL5.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_j6oac2?.base??"/grammar-cards-game",a=globalThis.__sveltekit_j6oac2?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DbxaRBN7.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1tdyhr?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1tdyhr?.assets??s??"";export{a,s as b};
