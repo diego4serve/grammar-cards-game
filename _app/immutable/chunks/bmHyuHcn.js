ed15287810fa165ef51aa480d900d32b56eb091a
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-19 14:55:11 -0500

    Fix Alex voice on macOS Safari + voice pre-caching

---
+++ _app/immutable/chunks/bmHyuHcn.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_8ce5v?.base??"/grammar-cards-game",a=globalThis.__sveltekit_8ce5v?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.CORugbI9.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BqO9iirl.js";export{o as load_css,r as start};
