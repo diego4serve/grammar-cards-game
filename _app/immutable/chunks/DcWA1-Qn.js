d0352eb2fe340336db889e2cf22dfc2e8095fcdd
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-19 15:49:46 -0500

    Move help button to tags row, make it more visible

---
+++ _app/immutable/chunks/DcWA1-Qn.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_so07x1?.base??"/grammar-cards-game",a=globalThis.__sveltekit_so07x1?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/vjllGGcz.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_322v0?.base??"/grammar-cards-game",a=globalThis.__sveltekit_322v0?.assets??s??"";export{a,s as b};
