85bf68693486199c6995ef3fede1e92562c1310b
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-19 22:43:57 -0500

    Strengthen prompt consistency: Spanish must match English exactly

---
+++ _app/immutable/chunks/BzLm_Squ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_zbvj0a?.base??"/grammar-cards-game",a=globalThis.__sveltekit_zbvj0a?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DcWA1-Qn.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_so07x1?.base??"/grammar-cards-game",a=globalThis.__sveltekit_so07x1?.assets??s??"";export{a,s as b};
