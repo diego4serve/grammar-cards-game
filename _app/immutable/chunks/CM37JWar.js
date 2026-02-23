3e04d9de48b466c441124240b257eefc0594bf7c
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-22 21:25:32 -0500

    Revert crypto.randomUUID fix

---
+++ _app/immutable/chunks/CM37JWar.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_lrj99?.base??"/grammar-cards-game",a=globalThis.__sveltekit_lrj99?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/CwRq8r4Q.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_17yovpu?.base??"/grammar-cards-game",a=globalThis.__sveltekit_17yovpu?.assets??s??"";export{a,s as b};
