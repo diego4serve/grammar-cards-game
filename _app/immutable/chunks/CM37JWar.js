b65abfaace82e39709118f1e4b76c05934a52949
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-02-22 21:32:00 -0500

    Fix crypto.randomUUID - use simple ID generator

--- _app/immutable/chunks/CM37JWar.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_lrj99?.base??"/grammar-cards-game",a=globalThis.__sveltekit_lrj99?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/CodPXEjB.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_vzie9c?.base??"/grammar-cards-game",a=globalThis.__sveltekit_vzie9c?.assets??s??"";export{a,s as b};
